d66e311c7290c655d8722c409c1e99eb2cbfd7e7
can <candanaciyan@gmail.com>
2024-05-04 19:11:05 +0300

    .

--- src/app/modules/product/product-sale/product-sale.component.ts
+++ src/app/modules/product/product-sale/product-sale.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { ProductService } from '../../../shared/service/product.service';
 import { LoginService } from '../../../core/service/login.service';
+import { FormBuilder } from '@angular/forms';
+import { Product } from '../../../shared/model/product';
 
 @Component({
   selector: 'app-product-sale',
@@ -10,6 +12,13 @@ import { LoginService } from '../../../core/service/login.service';
   styleUrl: './product-sale.component.scss'
 })
 export class ProductSaleComponent {
+  products: Product[] = [];
+  selectedProduct: Product | null = null;
+  totalAmount = 0;
+  saleform=this.fb.nonNullable.group({
+    count: 0,
+  });
+
 
   constructor(
     private router: Router,
@@ -17,7 +26,39 @@ export class ProductSaleComponent {
     private toastr: ToastrService,
     private productService: ProductService,
     private loginService: LoginService,
-  ) {}
-
+    private fb: FormBuilder,
+  ) { }
+  ngOnInit(): void {
+    this.refreshProducts();
+  }
+  refreshProducts() {
+    this.productService.getAllProducts().subscribe({
+      next: (result) => {
+        this.products = result;
+      }
+    });
+}
+productSelect(product: Product) {
+  this.selectedProduct = product;
+  this.productService.getProductCount(product.id).subscribe({
+    next: (data:any) => {
+      this.totalAmount = data.count;
+    }
+  });
+}
+submit() {
+  if (this.selectedProduct) {
+    let count = this.saleform.get('count')!.value;
+    this.productService.saleProduct(this.selectedProduct.id, count).subscribe({
+      next: (result) => {
+        this.toastr.info('Product sold');
+        this.router.navigate(['/menu']);
+      },
+      error: (err)=> {
+        this.toastr.error(err.error.mesaj);
+      }
+    });
+  }
+}
 
 }
