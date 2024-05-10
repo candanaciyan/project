50e58488b0fea8e9542f303905badf9a0efb6793
can <candanaciyan@gmail.com>
2024-05-10 13:36:25 +0300

    .

--- src/app/modules/user/user-create/user-create.component.ts
+++ src/app/modules/user/user-create/user-create.component.ts
@@ -7,18 +7,16 @@ import { UserService } from '../../../shared/service/user.service';
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
-  styleUrl: './user-create.component.scss'
+  styleUrls: ['./user-create.component.scss']
 })
 export class UserCreateComponent {
-  createForm = this.fb.nonNullable.group({
-    name: '',
-    surname: '',
-    email: ['',Validators.email],
-    password: '',
-    roleId: 0,
+  createForm = this.fb.group({
+    name: ['', Validators.required],
+    surname: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    roleId: [null, Validators.required], // null değeri seçilmemiş bir değer olarak başlangıçta atanıyor
   });
-
-
 
   constructor(
     private router: Router,
@@ -26,23 +24,23 @@ export class UserCreateComponent {
     public route: ActivatedRoute,
     private toastr: ToastrService,
     private userService: UserService,
-  ) { }
-
+  ) {}
 
   submit() {
-
-   this.userService.createUser({ ...this.createForm.value}).subscribe({
-     next: (resp) => {
-      console.log(resp);
-       this.toastr.success('User Created');
-       this.router.navigate(['..'], { relativeTo: this.route });
-       
-     },
-     error: (err) => {
-       console.log(err);
-       this.toastr.error("Please try again. ");
-     }
-   });
+    if (this.createForm.valid) {
+      this.userService.createUser({ ...this.createForm.value }).subscribe({
+        next: (resp) => {
+          console.log(resp);
+          this.toastr.success('User Created');
+          this.router.navigate(['..'], { relativeTo: this.route });
+        },
+        error: (err) => {
+          console.log(err);
+          this.toastr.error("Please try again.");
+        }
+      });
+    } else {
+      this.toastr.error("Please fill out all required fields.");
+    }
   }
-
 }
