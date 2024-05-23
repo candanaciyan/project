8cbd33bda0fa05393ff2217ee3f15d6068beaa77
can <candanaciyan@gmail.com>
2024-05-23 15:18:35 +0300

    .

--- src/app/shared/model/user.ts
+++ src/app/shared/model/user.ts
@@ -1,9 +1,12 @@
+import { Role } from "./role";
+
 export class User {
     constructor(
         public id: number,
         public name: string,
         public surname: string,
         public email: string,
+        public role: Role,
 
     ) {}
 }
